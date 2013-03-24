0262bb8919323ab3fdb3b3d4ac8155de5e6cc185
Klaus Trainer <klaus_trainer@posteo.de>
2013-03-24 23:01:38 +0100

    Remove unused macro

--- src/ssms_sup.erl
+++ src/ssms_sup.erl
@@ -9,9 +9,6 @@
 
 -include("ssms_srp.hrl").
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
 %% ===================================================================
 %% API functions
 %% ===================================================================
