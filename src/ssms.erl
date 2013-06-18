668bcc4856707d8164c671a7fb2f7437ade2d285
Klaus Trainer <klaus_trainer@posteo.de>
2013-06-18 16:53:40 +0200

    Remove unneeded matches

--- src/ssms.erl
+++ src/ssms.erl
@@ -4,9 +4,9 @@
 -export([start/0]).
 
 start() ->
-    ok = application:start(asn1),
-    ok = application:start(crypto),
-    ok = application:start(ranch),
-    ok = application:start(cowboy),
-    ok = application:start(sasl),
+    application:start(asn1),
+    application:start(crypto),
+    application:start(ranch),
+    application:start(cowboy),
+    application:start(sasl),
     ok = application:start(ssms).
