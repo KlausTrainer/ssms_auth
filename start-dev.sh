d21157f5c41038a1f0e0d14c11f28cc81a12324a
Klaus Trainer <klaus_trainer@posteo.de>
2013-07-09 12:56:24 +0200

    Rename 'reloader' to 'ssms_auth_reloader'

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 
-exec erl -pa erl -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s ssms_auth -s reloader
+exec erl -pa erl -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s ssms_auth -s ssms_auth_reloader
